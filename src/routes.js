c9455c75ddd9992e5df145b5a39c11781ab04849
Alan Nunes <alanunesouza@hotmail.com>
2019-10-29 23:16:08 -0300

    create users and init management plans

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
 import authMiddleware from './app/middlewares/auth';
+import StudentController from './app/controllers/StudentController';
 
 const routes = new Router();
 
@@ -13,5 +14,7 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.post('/students', StudentController.store);
 
 export default routes;
