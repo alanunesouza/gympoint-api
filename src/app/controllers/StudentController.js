f7e8c54757272a0aec5fd52df778ea04c7de5f88
Alan Souza <alan.souza@b2wdigital.com>
2019-11-12 13:46:25 -0300

    add registration routes

--- src/app/controllers/StudentController.js
+++ src/app/controllers/StudentController.js
@@ -45,6 +45,43 @@ class StudentController {
       height,
     });
   }
+
+  async update(req, res) {
+    const schema = Yup.object().shape({
+      student_id: Yup.number().required(),
+      name: Yup.string(),
+      email: Yup.string().email(),
+      age: Yup.number().positive(),
+      weight: Yup.number().positive(),
+      height: Yup.number().positive(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    if (req.body.email) {
+      const studentExists = await Student.findOne({
+        where: { email: req.body.email },
+      });
+
+      if (studentExists) {
+        return res
+          .status(400)
+          .json({ error: 'This e-mail is already registered' });
+      }
+    }
+
+    const student = await Student.findByPk(req.body.student_id);
+
+    if (!student) {
+      return res.status(404).json({ error: 'Student not found' });
+    }
+
+    await student.update(req.body);
+
+    return res.json(student);
+  }
 }
 
 export default new StudentController();
--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import SessionController from './app/controllers/SessionController';
 import authMiddleware from './app/middlewares/auth';
 import StudentController from './app/controllers/StudentController';
 import PlanController from './app/controllers/PlanController';
+import RegistrationController from './app/controllers/RegistrationController';
 
 const routes = new Router();
 
@@ -16,11 +17,17 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 
+routes.post('/students', StudentController.store);
+routes.put('/students', StudentController.update);
+
 routes.post('/plans', PlanController.store);
 routes.get('/plans', PlanController.index);
 routes.put('/plans/:planId', PlanController.update);
 routes.delete('/plans/:planId', PlanController.delete);
 
-routes.post('/students', StudentController.store);
+routes.post('/registrations', RegistrationController.store);
+routes.get('/registrations', RegistrationController.index);
+routes.put('/registrations/:registrationId', RegistrationController.update);
+routes.delete('/registrations/:registrationId', RegistrationController.delete);
 
 export default routes;
